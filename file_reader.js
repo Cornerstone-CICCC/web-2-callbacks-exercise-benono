7dfc2069debc406abc8f2b4356f7f7fcab7911f5
BenOno <bentoki.1213@gmail.com>
2024-07-23 16:22:56 -0700

    Finish

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,15 @@
-const fs = require("fs");
+const fs = require("fs")
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+fs.readFile("./firstname.txt", "utf-8", (err, firstname) => {
+  process.stdout.write(firstname)
+  fs.readFile("./lastname.txt", "utf-8", (err, lastname) => {
+    process.stdout.write(` ${lastname}`)
+    fs.readFile("./age.txt", "utf-8", (err, age) => {
+      process.stdout.write(` is ${age} years old`)
+    })
+    fs.readFile("./hobbies.txt", "utf-8", (err, content) => {
+      const hobby = JSON.parse(content)
+      process.stdout.write(` and his hobbies are ${hobby[0]} and ${hobby[1]}\n`)
+    })
+  })
 })
